62f85e7bd98a7776e221eecb8b0de6f0044631f0
Sambud <35185764+shambhavibudholiya@users.noreply.github.com>
2020-05-03 20:06:07 +0530

    updates

--- src/Navigation.js
+++ src/Navigation.js
@@ -18,7 +18,7 @@ const Navigation = () =>
   <header className="Navigation dt--fixed border-box bb b--black-10">
     <nav className="nav pa4-ns">
       <a className="yt dim black f4 f5-ns mr4" href="https://www.youtube.com/channel/UC-19kU4aNJRS77wRxo0uPHg" title="Watch on Youtube">YouTube</a>
-      <NavLink to="/" className="as dim red f4 f5-ns mr4">About</NavLink>
+      <NavLink to="/webbing-dad" className="as dim red f4 f5-ns mr4">Home</NavLink>
       <NavLink to="/Contact" className="as dim red f4 f5-ns mr4">Contact</NavLink>
     </nav>
      
@@ -26,37 +26,12 @@ const Navigation = () =>
 
 
      </div>
-     <Route exact path="/" component={Page} />
-      <Route exact path="/" component={Slide} />
-      <Route exact path="/" component={Grid} />
-      <Route path="/Contact" component={Form } />
+     <Route exact path="/webbing-dad" component={Page} />
+      <Route exact path="/webbing-dad" component={Slide} />
+      <Route exact path="/webbing-dad" component={Grid} />
+      <Route exact path="/Contact" component={Form } />
     </div>
   </Router>
 );
 export default Navigation;
 
-
-// <Router><div>
-// //   <header className="Navigation dt--fixed border-box bb b--black-10">
-// //     <nav className="nav pa4-ns">
-// //       <a className="yt dim black f4 f5-ns mr4" href="https://www.youtube.com/channel/UC-19kU4aNJRS77wRxo0uPHg" title="Watch on Youtube">YouTube</a>
-// //       {/* <a className="as dim red f4 f5-ns mr4" href="/" title="About">About</a> 
-// //       <a className="as dim red f4 f5-ns mr4" href="/" title="About">About</a>  */}
-
-// //       <Link to="/Contact">Contact</Link>
-
-// //       {/* <NavLink to="/about" className="as dim red f4 f5-ns mr4">About</NavLink>
-// //       <NavLink to="/Contact" className="as dim red f4 f5-ns mr4">Contact</NavLink> */}
-// //     </nav>
-// // <Route path="/Contact" component = {Form} exact/> 
-// // <Route path="/" component = {Page } /> 
-// // <Route path="/" component = {Slide}  />
-// // <br />
-// // <br />
-// // <br />
-// // <Route path="/" component = {Grid}/> 
-// //      </header>
-
-
-// //      </div>
-// //      </Router> 
