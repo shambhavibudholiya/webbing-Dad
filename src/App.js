bcc883d9ee6b8319a0dea336b365a21f70c17592
shambhavibudholiya <shambhavi20.1998@gmail.com>
2020-05-03 19:40:31 +0530

    Updating contact

--- src/App.js
+++ src/App.js
@@ -10,27 +10,14 @@ import './Navigation.css';
 import BackToTop from "react-back-to-top-button";
 
 
+
 class App extends Component {
 render(){
 return (
 
-<BrowserRouter>
 
 <div className="App">
 <Navigation />   
-<Page/>
-<Slide />
-<Grid/>
-
-<Route path="/Form" component = {Form}/> 
-{/* <Route path="" component = {Page } exact/> 
-<Route path="" component = {Slide} exact />
-<br />
-<br />
-<br />
-<Route path="" component = {Grid} exact/>   */}
-
-
   <BackToTop
   showOnScrollUp
   showAt={100}
@@ -38,15 +25,26 @@ return (
   easing="easeInOutQuint"
 >
 <span>
-
 <img width="80px" height="80px" src="https://www.decalsplanet.com/img_b/vinyl-decal-sticker-8136.jpg"></img>
-
 </span>
+
 </BackToTop>
 
   </div>
-</BrowserRouter>
 );
 }
 }
   export default App;
+
+
+//   <Page/>
+// <Slide />
+// <Grid/> 
+/* 
+<Route path="/Form" component = {Form}/> 
+<Route path="" component = {Page } exact/> 
+<Route path="" component = {Slide} exact />
+<br />
+<br />
+<br />
+<Route path="" component = {Grid} exact/>   */
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,24 +1,62 @@
 import React from 'react';  
 import './Navigation.css';
-import Contact from './Contact';
-import { NavLink } from 'react-router-dom';
+import Form from './Contact';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import Page from './Page';
+import Slide from './SlideEffect'; 
+import Grid from './Grid';
 
 
-const Navigation = props =>(
 
+
+const Navigation = () =>
+(
+  <Router>
     <div>
+      <div>
   <header className="Navigation dt--fixed border-box bb b--black-10">
     <nav className="nav pa4-ns">
       <a className="yt dim black f4 f5-ns mr4" href="https://www.youtube.com/channel/UC-19kU4aNJRS77wRxo0uPHg" title="Watch on Youtube">YouTube</a>
-      {/* <a className="as dim red f4 f5-ns mr4" href="https://mailchi.mp/a8b8bb3ecbc6/yogacharyaajay" title="Contact">Contact1</a>  */}
-
-      
-      <NavLink to="/about" className="as dim red f4 f5-ns mr4">About</NavLink>
-      <NavLink to="/Form" className="as dim red f4 f5-ns mr4">Contact</NavLink>
+      <NavLink to="/" className="as dim red f4 f5-ns mr4">About</NavLink>
+      <NavLink to="/Contact" className="as dim red f4 f5-ns mr4">Contact</NavLink>
     </nav>
+     
      </header>
+
+
      </div>
-
+     <Route exact path="/" component={Page} />
+      <Route exact path="/" component={Slide} />
+      <Route exact path="/" component={Grid} />
+      <Route path="/Contact" component={Form } />
+    </div>
+  </Router>
 );
-
 export default Navigation;
+
+
+// <Router><div>
+// //   <header className="Navigation dt--fixed border-box bb b--black-10">
+// //     <nav className="nav pa4-ns">
+// //       <a className="yt dim black f4 f5-ns mr4" href="https://www.youtube.com/channel/UC-19kU4aNJRS77wRxo0uPHg" title="Watch on Youtube">YouTube</a>
+// //       {/* <a className="as dim red f4 f5-ns mr4" href="/" title="About">About</a> 
+// //       <a className="as dim red f4 f5-ns mr4" href="/" title="About">About</a>  */}
+
+// //       <Link to="/Contact">Contact</Link>
+
+// //       {/* <NavLink to="/about" className="as dim red f4 f5-ns mr4">About</NavLink>
+// //       <NavLink to="/Contact" className="as dim red f4 f5-ns mr4">Contact</NavLink> */}
+// //     </nav>
+// // <Route path="/Contact" component = {Form} exact/> 
+// // <Route path="/" component = {Page } /> 
+// // <Route path="/" component = {Slide}  />
+// // <br />
+// // <br />
+// // <br />
+// // <Route path="/" component = {Grid}/> 
+// //      </header>
+
+
+// //      </div>
+// //      </Router> 
